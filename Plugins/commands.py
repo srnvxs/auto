58078e5fa418cace43b79e13a632af137fc7454e
[Oʀᴇᴡᴀ_Sʀɪɴɪᴠᴀs]²ᵘ <104828717+srnvxs@users.noreply.github.com>
2025-11-27 13:40:38 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -5,16 +5,13 @@
 import logging
 logger = logging.getLogger(__name__)
 
-from pyrogram import filters
-from bot import channelforward
+from pyrogram import Client, filters
 from config import Config
 from translation import Translation
 
-
-################################################################################################################################################################################################################################################
+###################################################################
 # start command
-
-@channelforward.on_message(filters.command("start") & filters.private & filters.incoming)
+@Client.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
     await message.reply(
         text=Translation.START,
@@ -22,15 +19,12 @@ async def start(client, message):
         quote=True
     )
 
-################################################################################################################################################################################################################################################
+###################################################################
 # about command
-
-@channelforward.on_message(filters.command("about") & filters.private & filters.incoming)
+@Client.on_message(filters.command("about") & filters.private & filters.incoming)
 async def about(client, message):
     await message.reply(
         text=Translation.ABOUT,
         disable_web_page_preview=True,
         quote=True
     )
-
-################################################################################################################################################################################################################################################
